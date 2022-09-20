d5005dbfc8635bbd6a35dac0900c96a9121a2eeb
DESKTOP-K6DD1JI\roytravel <roytravel97@gmail.com>
2022-09-20 16:50:50 +0900

    Modularize the alexnet

--- alexnet/alexnet.py
+++ alexnet/alexnet.py
@@ -1,16 +1,8 @@
-import os
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
-from torch.utils import data
 #from tqdm import tqdm
-import logging
-from glob import glob
 
-logging.basicConfig(level=logging.INFO)
 class AlexNet(nn.Module):
     def __init__(self, num_classes=1000):
         super().__init__()
@@ -65,83 +57,3 @@ class AlexNet(nn.Module):
         x = torch.flatten(x, 1) # 1차원화
         x = self.classifier(x)
         return x
-
-
-if __name__ == "__main__":
-    #parser = argparse.ArgumentParser(add_help=True)
-    seed = torch.initial_seed()
-    print (f'[*] Seed : {seed}')
-    NUM_EPOCHS = 1000
-    BATCH_SIZE = 128
-    NUM_CLASSES = 1000
-    LEARNING_RATE = 0.00005 # 1) 0.0001 2) 0.00001
-    IMAGE_SIZE = 227
-    TRAIN_IMG_DIR = "C:/github/paper-implementation/data/ILSVRC2012_img_train/"
-    #VALID_IMG_DIR = "<INPUT VALID IMAGE DIR>"
-    CHECKPOINT_PATH = "./checkpoint/"
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print (f'[*] Device : {device}')
-
-    alexnet = AlexNet(num_classes=NUM_CLASSES).cuda()
-    checkpoints = glob(CHECKPOINT_PATH+'*.pth') # Is there a checkpoint file?
-    if checkpoints:
-        checkpoint = torch.load(checkpoints[-1])
-        alexnet.load_state_dict(checkpoint['model'])
-    #alexnet = torch.nn.parallel.DataParallel(alexnet, device_ids=[0,]) # for distributed training using multi-gpu
-
-    transform = transforms.Compose(
-        [transforms.CenterCrop(IMAGE_SIZE),
-         transforms.RandomHorizontalFlip(),
-         transforms.ToTensor(),
-         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
-        
-    train_dataset = datasets.ImageFolder(TRAIN_IMG_DIR, transform=transform)
-    print ('[*] Dataset Created')
-
-    train_dataloader = data.DataLoader(
-        train_dataset,
-        shuffle=True,
-        pin_memory=False, # more training speed but more memory
-        num_workers=8,
-        drop_last=True,
-        batch_size=BATCH_SIZE
-    )
-    print ('[*] DataLoader Created')
-
-    #optimizer = torch.optim.SGD(momentum=0.9, weight_decay=5e-4, params=alexnet.parameters(), lr=LEARNING_RATE) # SGD used in original paper
-    optimizer = torch.optim.Adam(params=alexnet.parameters(), lr=LEARNING_RATE)
-    print ('[*] Optimizer Created')
-
-    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, factor=0.1, verbose=True, patience=4) # used if valid error doesn't improve.
-    print ('[*] Learning Scheduler created')
-
-    steps = 1
-    for epoch in range(50, NUM_EPOCHS):
-        logging.info(f" training on epoch {epoch}...")        
-        for batch_idx, (images, classes) in enumerate(train_dataloader):
-            images, classes = images.cuda(), classes.cuda()
-            output = alexnet(images)
-            loss = F.cross_entropy(input=output, target=classes)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            if steps % 50 == 0:
-                with torch.no_grad():
-                    _, preds = torch.max(output, 1)
-                    accuracy = torch.sum(preds == classes)
-                    print ('[*] Epoch: {} \tStep: {}\tLoss: {:.4f} \tAccuracy: {}'.format(epoch+1, steps, loss.item(), accuracy.item() / BATCH_SIZE))
-            steps = steps + 1
-
-        lr_scheduler.step(metrics=loss)
-
-        if epoch % 5 == 0:
-            checkpoint_path = os.path.join(CHECKPOINT_PATH, "model_{}.pth".format(epoch))
-            state = {
-                'epoch': epoch,
-                'optimizer': optimizer.state_dict(),
-                'model': alexnet.state_dict(),
-                'seed': seed
-            }
-            torch.save(state, checkpoint_path)
---
+++ alexnet/train.py
@@ -0,0 +1,91 @@
+import os
+import argparse
+import logging
+import torch
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils import data
+from glob import glob
+from alexnet.alexnet import AlexNet
+
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('--args.num_epochs', default=1000, type=int, required=False)
+    parser.add_argument('--batch_size', default=128, type=int, required=False)
+    parser.add_argument('--learning_rate', default=0.00005, type=float, required=False)
+    args = parser.parse_args()
+
+    seed = torch.initial_seed()
+    print (f'[*] Seed : {seed}')
+    NUM_CLASSES = 1000
+    TRAIN_IMG_DIR = "C:/github/paper-implementation/data/ILSVRC2012_img_train/"
+    #VALID_IMG_DIR = "<INPUT VALID IMAGE DIR>"
+    CHECKPOINT_PATH = "../checkpoint/"
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print (f'[*] Device : {device}')
+
+    alexnet = AlexNet(num_classes=NUM_CLASSES).to(device)
+    checkpoints = glob(CHECKPOINT_PATH+'*.pth') # Is there a checkpoint file?
+    if checkpoints:
+        checkpoint = torch.load(checkpoints[-1])
+        alexnet.load_state_dict(checkpoint['model'])
+    #alexnet = torch.nn.parallel.DataParallel(alexnet, device_ids=[0,]) # for distributed training using multi-gpu
+
+    transform = transforms.Compose(
+        [transforms.CenterCrop(227),
+         transforms.RandomHorizontalFlip(),
+         transforms.ToTensor(),
+         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        
+    train_dataset = datasets.ImageFolder(TRAIN_IMG_DIR, transform=transform)
+    print ('[*] Dataset Created')
+
+    train_dataloader = data.DataLoader(
+        train_dataset,
+        shuffle=True,
+        pin_memory=False, # more training speed but more memory
+        num_workers=8,
+        drop_last=True,
+        batch_size=args.batch_size
+    )
+    print ('[*] DataLoader Created')
+
+    #optimizer = torch.optim.SGD(momentum=0.9, weight_decay=5e-4, params=alexnet.parameters(), lr=args.learning_rate) # SGD used in original paper
+    optimizer = torch.optim.Adam(params=alexnet.parameters(), lr=args.learning_rate)
+    print ('[*] Optimizer Created')
+
+    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, factor=0.1, verbose=True, patience=4) # used if valid error doesn't improve.
+    print ('[*] Learning Scheduler created')
+
+    for epoch in range(50, args.num_epochs):
+        logging.info(f" training on epoch {epoch}...")        
+        for batch_idx, (images, classes) in enumerate(train_dataloader):
+            images, classes = images.to(device), classes.to(device)
+            output = alexnet(images)
+            loss = F.cross_entropy(input=output, target=classes)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if batch_idx % 50 == 0:
+                with torch.no_grad():
+                    _, preds = torch.max(output, 1)
+                    accuracy = torch.sum(preds == classes)
+                    print ('[*] Epoch: {} \tStep: {}\tLoss: {:.4f} \tAccuracy: {}'.format(epoch+1, batch_idx, loss.item(), accuracy.item() / args.batch_size))
+
+        lr_scheduler.step(metrics=loss)
+
+        if epoch % 5 == 0:
+            checkpoint_path = os.path.join(CHECKPOINT_PATH, "model_{}.pth".format(epoch))
+            state = {
+                'epoch': epoch,
+                'optimizer': optimizer.state_dict(),
+                'model': alexnet.state_dict(),
+                'seed': seed
+            }
+            torch.save(state, checkpoint_path)
